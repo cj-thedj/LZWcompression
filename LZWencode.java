a27ab532b5d0d545a203d7681bee84435781698f
Transfield, Harmon Kurt <h.k.transfield1@gmail.com>
2022-03-21 22:30:12 +1300

    feat: add functional compression with working trie
    The trie and compression now fully work. The trie only works with this specific
    implementation. The LZW works correctly.

--- LZWencode.java
+++ LZWencode.java
@@ -1,33 +1,82 @@
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.BufferedInputStream;
+import java.util.ArrayList;
 
 /**
  * The encoder, with LZW algorithm:
- *  - Takes input as a stream of hex digits (chars) from stdin
- *  - Outputs phrase number, one phrase number per line
- *  - Utilise a multiway trie
+ * - Takes input as a stream of hex digits (chars) from stdin
+ * - Outputs phrase number, one phrase number per line
+ * - Utilise a multiway trie
  */
 public class LZWencode {
-    public static void main(String[] args) {       
+    public static void main(String[] args) {
         try {
             File file = new File("input.txt");
             FileInputStream fis = new FileInputStream(file);
             BufferedInputStream bis = new BufferedInputStream(fis);
+            ArrayList<Integer> nibbles = new ArrayList<Integer>();
 
-            byte[] byteArray = null;
-            
-            byteArray = bis.readAllBytes();
+            byte[] byteArray = bis.readAllBytes();
 
-             // Iterating over using for each loop
+            // instantiate a new trie that has 1st level symbols
+            Trie t = new Trie();
+            Trie.TrieNode parentNode = t.getRoot();
+            Trie.TrieNode childNode, tempNode;
+
+            t.print();
+            System.out.println("---Phrase Numbers---");
+
+            // extract each 'nibble' or hexadecimal digit
             for (byte b : byteArray) {
-                String st = String.format("%02X", b);
-                System.out.println((char)b + " = " + st);
-            // Printing the content of byte array
-            
-        }
+                int highNibble = (b >> 4) & 0x0f;
+                nibbles.add(highNibble);
 
-        bis.close();
+                int lowNibble = b & 0x0f;
+                nibbles.add(lowNibble);
+            }
 
-        } catch (Exception e) {
+            tempNode = parentNode;
+            int tempNibble = 0;
+            int phraseStart = 0;
+
+            // check if each nibble is in the trie
+            for (int nibble : nibbles) {
+
+                // search for matching phrases
+                for (int i = phraseStart; i < nibbles.size(); i++) {
+
+                    tempNibble = nibbles.get(i);
+
+                    // save current state of parent
+                    tempNode = parentNode;
+
+                    // search if parent's children have nibble
+                    childNode = t.search(tempNibble, parentNode);
+
+                    // if not null, continue to the next nibble
+                    if (childNode != null) {
+
+                        // if child has children, it becomes new parent
+                        parentNode = childNode;
+                        continue;
+
+                    } else
+                        break;
+                }
+
+                System.out.println(parentNode.getPhraseNum());
+                t.insert(nibble, parentNode);
+
+                // start back at the root
+                parentNode = t.getRoot();
+                phraseStart++;
+            }
+
+            System.out.print("\n");
+            bis.close();
+
+        } catch (exception e) {
             System.err.println(e);
         }
     }
--- Trie.java
+++ Trie.java
@@ -1,5 +1,122 @@
-public class Trie {
-    private class TrieNode {
-        
+
+/**
+ * Harmon Transfield and Caleb Pyne
+ */
+
+import java.util.ArrayList;
+
+class Trie {
+    // define alphabet size (0 - F)
+    private static final int HEX_SET = 16; // 0, 1, 2, 3, 4, 5, 6, 7,8, 9, A, B, C, D, E, F
+    private static TrieNode _root;
+    private static int _totalNodes;
+    ////////////////////////////////////////////////////////////////////////////////
+
+    class TrieNode {
+        private ArrayList<TrieNode> _children = new ArrayList<TrieNode>();
+        private boolean _isLeaf;
+        private int _phraseNumber;
+        private int _phraseNibble;
+
+        /**
+         * Constructor
+         * 
+         * Used for instatiating root nodes of a Trie.
+         */
+        TrieNode() {
+            _totalNodes++;
+            _isLeaf = false;
+        }
+
+        /**
+         * Overloaded Constructor.
+         * 
+         * Used to instatiate
+         * 
+         * @param hexNibble
+         * @param phraseNumber
+         */
+        TrieNode(int hexNibble, int phraseNumber) {
+            _totalNodes++;
+            _isLeaf = false;
+            _phraseNibble = hexNibble;
+            _phraseNumber = phraseNumber;
+
+        }
+
+        public ArrayList<TrieNode> getChildren() {
+            return _children;
+        }
+
+        public int getPhraseNum() {
+            return _phraseNumber;
+        }
+
+        public int getPhraseHexDigit() {
+            return _phraseNibble;
+        }
+
+        public boolean getIsLeaf() {
+            return _isLeaf;
+        }
+
+        public void setChildren(int hexDigit) {
+            _children.add(new TrieNode(hexDigit, _totalNodes));
+            this._isLeaf = false;
+        }
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * Constructor
+     * 
+     * Instatiates a new Trie with a root node.
+     */
+    public Trie() {
+        _totalNodes = 0;
+        _root = new TrieNode();
+
+        // initialize trie with expected symbols
+        for (int i = 0; i < HEX_SET; i++) {
+
+            _root.setChildren(i);
+        }
+    }
+
+    public TrieNode search(int nibble, TrieNode currNode) {
+
+        System.out.println("Nibble = " + nibble);
+
+        // search if one of its children has the key
+        for (int i = 0; i < currNode.getChildren().size(); i++) {
+            if (currNode.getChildren().get(i).getPhraseHexDigit() == nibble)
+                return currNode.getChildren().get(i);
+        }
+
+        return null;
+    }
+
+    // prints out all current nodes and associated phrases
+    public void print() {
+        int testChar;
+        int testNum;
+
+        for (int i = 0; i < _root.getChildren().size(); i++) {
+            testChar = _root.getChildren().get(i).getPhraseHexDigit();
+            testNum = _root.getChildren().get(i).getPhraseNum();
+
+            String stx = String.format("%2X", testChar);
+
+            System.out.println(stx + " = " + testNum);
+        }
+    }
+
+    public TrieNode getRoot() {
+        return _root;
+    }
+
+    public void insert(int hexDigit, TrieNode parentNode) {
+        parentNode.setChildren(hexDigit);
     }
 }
